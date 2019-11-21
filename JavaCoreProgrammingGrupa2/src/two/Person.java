55b2b68329b5b4d60eff3969ad58ce8d5a10f721
Grupa2 <Grupa2@Predavacki-04>
2019-11-21 18:19:07 +0100

    i2345 kreiramo sam nasu famoznu Personu u paketu two

---
+++ JavaCoreProgrammingGrupa2/src/two/Person.java
@@ -0,0 +1,30 @@
+package two;
+
+/**
+ *
+ * @author EmirPuska
+ */
+public class Person {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String ime, String prezime, int mladost) {
+        this.name = ime;
+        this.surname = prezime;
+        this.age = mladost;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
