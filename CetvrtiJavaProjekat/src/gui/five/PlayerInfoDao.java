8138a346a090aa089087764174e3fbcb5b2749bd
Grupa2 <Grupa2@Predavacki-04>
2020-04-30 17:22:45 +0200

    Fix wrong usage of connectio pool object

--- CetvrtiJavaProjekat/src/gui/five/PlayerInfoDao.java
+++ CetvrtiJavaProjekat/src/gui/five/PlayerInfoDao.java
@@ -1,6 +1,7 @@
 package gui.five;
 
 import advanced.konsultacije.Dao;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -27,13 +28,14 @@ public class PlayerInfoDao implements Dao<PlayerInfo> {
     public List<PlayerInfo> getAll() {
         String sqlQuery = "SELECT * FROM PUSKA.PLAYERINFO FETCH FIRST 100 ROWS ONLY";
         List<PlayerInfo> listOfPlayers = new ArrayList<>();
-
-        try (PreparedStatement ps = connectionPool.getConnection().prepareStatement(sqlQuery);) {
+        Connection connection = connectionPool.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(sqlQuery);) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 PlayerInfo playerInfo = new PlayerInfo(rs.getInt("id"), rs.getString("firstName"), rs.getString("lastName"), rs.getString(4), rs.getInt(5), rs.getBoolean(6));
                 listOfPlayers.add(playerInfo);
             }
+            connectionPool.releaseConnection(connection);
             return listOfPlayers;
         } catch (SQLException ex) {
             throw new RuntimeException(ex.getMessage());
@@ -42,7 +44,8 @@ public class PlayerInfoDao implements Dao<PlayerInfo> {
 
     public Vector getColumnNames() {
         String sqlQuery = "SELECT * from playerinfo";
-        try (PreparedStatement ps = connectionPool.getConnection().prepareStatement(sqlQuery);
+        Connection connection = connectionPool.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(sqlQuery);
                 ResultSet rs = ps.executeQuery();) {
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             int columns = resultSetMetaData.getColumnCount();
@@ -50,6 +53,7 @@ public class PlayerInfoDao implements Dao<PlayerInfo> {
             for (int i = 1; i <= columns; i++) {
                 columnNames.addElement(resultSetMetaData.getColumnName(i));
             }
+            connectionPool.releaseConnection(connection);
             return columnNames;
         } catch (SQLException ex) {
             throw new RuntimeException(ex.getMessage());
@@ -65,10 +69,12 @@ public class PlayerInfoDao implements Dao<PlayerInfo> {
     public void update(PlayerInfo entity)  {
         System.out.println(entity);
         String sql = "UPDATE PLAYERINFO SET SPORT=? WHERE id=?";
-        try(PreparedStatement ps = connectionPool.getConnection().prepareStatement(sql)){
+        Connection connection = connectionPool.getConnection();
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setString(1, entity.getSport());
             ps.setInt(2, entity.getId());
             ps.execute();
+            connectionPool.releaseConnection(connection);
         }catch(SQLException e){
             throw new RuntimeException(e.getMessage());
         }
